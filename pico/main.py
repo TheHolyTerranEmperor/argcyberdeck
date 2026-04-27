43aa994fd828f773e09ff3e219a1a2bc52d89c66
Sable Evans <brodyevans82@gmail.com>
2026-04-27 12:10:40 -0600

    added main file for pico
    starting the main pico file for controlling the cyberdeck

---
+++ pico/main.py
@@ -0,0 +1,11 @@
+import board
+import keypad
+import usb_hid
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keycode import Keycode
+
+
+#pinouts and assigning GPIO (assigning rows and cols)
+
+
+#Keycodes for buttons
