713e4f5628099fe651420a2fc6078b2fdf151f6c
Sable Evans <brodyevans82@gmail.com>
2026-04-27 14:22:20 -0600

    Update main.py
    moved selection keys for better PCB layout

--- pico/main.py
+++ pico/main.py
@@ -6,33 +6,36 @@ from adafruit_hid.keycode import Keycode
 
 
 #pinouts and assigning GPIO (assigning rows and cols)
+
+#_________________________________
 COL_PINS = (board.GP0, board.GP1, board.GP2)
-ROW_PINS = (board.GP3, board.GP4, board.GP5, board.GP6, board.GP7, board.GP8)
-
-#Keycodes for buttons
-
+ROW_PINS = (board.GP3, board.GP4, board.GP5, board.GP6)
 KEYCODES = (
     Keycode.SEVEN, Keycode.EIGHT, Keycode.NINE
     Keycode.FOUR, Keycode.FIVE, Keycode.SIX,
     Keycode.ONE, Keycode.TWO, Keycode.THREE,
     Keycode.KEYPAD_ASTERISK, Keycode.ZERO, Keycode.POUND
-    Keycode.W, Keycode.S, Keycode.T, 
-    Keycode.A, Keycode.D, Keycode.G
+
 )
 keys = keypad.KeyMatrix(ROW_PINS, COL_PINS)
-
-
+#_________________________________
 SPECIAL_COL_PINS = (board.GP16, board.GP17, board.GP18)
 SPECIAL_ROW_PINS = (board.GP19, board.GP20, board.GP21, board.GP22)
-
-
 SPECIAL_KEYCODES = (
     Keycode.Y, Keycode.U, Keycode.I,
     Keycode.H, Keycode.J, Keycode.K,
     Keycode.N, Keycode.M, Keycode.L
 )
-
-
+special_keys = keypad.KeyMatrix(SPECIAL_ROW_PINS, SPECIAL_COL_PINS)
+#_________________________________
+SELECT_COL_PINS = (board.GP13, board.GP14, board.GP15)
+SELECT_ROW_PINS = (board.GP7, board.GP8)
+SELECT_KEYCODES = (
+    Keycode.W, Keycode.S, Keycode.T, 
+    Keycode.A, Keycode.D, Keycode.G
+)
+select_keys = keypad.KeyMatrix(SELECT_ROW_PINS, SELECT_COL_PINS)
+#_________________________________
 
 kbd = Keyboard(usb_hid.devices)
 
@@ -44,6 +47,7 @@ while True:
             kbd.press(KEYCODES[key_number])
         if event.released:
             kbd.release(KEYCODES[key_number])
+            
     special_event = special_keys.events.get()
     if special_event:
         special_key_number = special_event.key_number
@@ -51,3 +55,11 @@ while True:
             kbd.press(SPECIAL_KEYCODES[special_key_number])
         if special_event.released:
             kbd.release(SPECIAL_KEYCODES[special_key_number])
+            
+    select_event = select_keys.events.get()
+    if special_event:
+        special_key_number = select_event.key_number
+        if select_event.pressed:
+            kbd.press(SELECT_KEYCODES[select_key_number])
+        if select_event.released:
+            kbd.release(SELECT_KEYCODES[select_key_number])
